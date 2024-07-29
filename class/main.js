a08adde21040b3fcf6906aaccaf68a04c0dc41cd
Varshitha Panuganti <vpanuganti.1283@gmail.com>
2024-07-29 16:19:14 -0500

    create main.js

---
+++ class/main.js
@@ -0,0 +1,16 @@
+// main.js
+// camelCasing- camel casing (Main casing will be used in Javascript)
+// kebob-casing- kebob casing
+// snake_casing
+
+//Compiler
+// JS (interpreted language,interpreter program) -> interpreter executes the js code line by line
+// garbage collection: for memory management
+
+//  java/C# compiled /interpreted -> bytecode -> ported to different computer (JVM)
+// -> executes intermediate bytecode or compiles into machie code -> runs it
+// garbage collection
+
+// C/C++ compiled-> machinne language -> cpu executes
+// No garbage collection-manual memory management
+// low  energy cost/highly efficient / fast runtime / lower memory
