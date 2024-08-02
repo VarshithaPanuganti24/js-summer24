b387d0f94a68df237e67b83673265ad6d94f6bf7
Varshitha Panuganti <vpanuganti.1283@gmail.com>
2024-08-02 16:31:16 -0500

    array,binary,compare

---
+++ class/array.js
@@ -0,0 +1,10 @@
+//Create an array
+const arrA =[];
+console.log(arrA);
+
+//update an array
+arrA[1] = 999;
+arrA[2] = 561;
+arrA.push(1000)
+console.log(arrA);
+//console.log(arrA[0]);
---
+++ class/compare.js
@@ -0,0 +1,17 @@
+//compare values
+//one qual sign :assignment operator
+
+//double equal sign: convert into same type (if diff) then compare
+console.log("7" == 7);
+console.log(7==7);
+console.log(8===7);
+
+
+//triple equal sign:compare type(if diff type, returns false) then comares
+console.log("7" === 7);
+console.log(7===7);
+console.log(8===7);
+
+//other primitives
+console.log(0 === false);
+console.log (0 == false);
