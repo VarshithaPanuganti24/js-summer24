618e31b2474b227922a14bf956c8f039137f38fb
Varshitha Panuganti <vpanuganti.1283@gmail.com>
2024-08-05 16:04:38 -0500

    array

--- class/array.js
+++ class/array.js
@@ -1,10 +1,36 @@
 //Create an array
 const arrA =[];
-console.log(arrA);
+ console.log(arrA);
 
 //update an array
 arrA[1] = 999;
 arrA[2] = 561;
-arrA.push(1000)
+arrA.push(1000);
+arrA.push(1001,1002, 1003);
+arrA.push([1005,1006,1007]);
+arrA[8] = {};
+
 console.log(arrA);
-//console.log(arrA[0]);
+console.log(arrA[0]);
+console.log(arrA.length);
+
+//build array,start a inndex 1, add new item 7 ,try pushing some items to the array
+
+// const checkA =[];
+// //console.log(checkA);
+
+// checkA[1]= 76;
+// checkA[7]=678;
+// //console.log(checkA);
+// checkA[4]= 432;
+// checkA.push(444);
+// checkA.push([2,7,899]);
+// checkA[0]=88;
+// checkA[8] =7654;
+// //console.log(checkA);
+// //console.log(checkA[5]);
+// console.log(checkA);
+
+//const itemRemoved =arrA.pop90;\
+arrA.unshift(8);
+console.log(arrA);
--- class/compare.js
+++ class/compare.js
@@ -2,15 +2,15 @@
 //one qual sign :assignment operator
 
 //double equal sign: convert into same type (if diff) then compare
-console.log("7" == 7);
-console.log(7==7);
-console.log(8===7);
+console.log("7" == 7);//true
+console.log(7==7);//true
+console.log(8===7);//false
 
 
 //triple equal sign:compare type(if diff type, returns false) then comares
-console.log("7" === 7);
-console.log(7===7);
-console.log(8===7);
+console.log("7" === 7);//false
+console.log(7===7);//true
+console.log(8===7);//false
 
 //other primitives
 console.log(0 === false);
