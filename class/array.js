5411848fbd9994c92a3f28da6a498cacef6633c0
Varshitha Panuganti <vpanuganti.1283@gmail.com>
2024-08-06 16:32:19 -0500

    array

--- class/array.js
+++ class/array.js
@@ -1,18 +1,48 @@
 //Create an array
-const arrA =[];
- console.log(arrA);
+//const arrA =[];
+ //console.log(arrA);
 
 //update an array
-arrA[1] = 999;
-arrA[2] = 561;
-arrA.push(1000);
-arrA.push(1001,1002, 1003);
-arrA.push([1005,1006,1007]);
-arrA[8] = {};
+// arrA[1] = 999;
+// arrA[7] = 561;
+// arrA.push(1000);
+// arrA.push(1001,1002, 1003);
+// arrA.push([1005,1006,1007]);
+// //arrA[8] = {};
 
-console.log(arrA);
-console.log(arrA[0]);
-console.log(arrA.length);
+// console.log("using loop");
+// const fruits =["gr","ap","ba"];
+
+// function loopPrint(array){
+
+//     for (let i=0 ;i < array.length;i++){
+//     const element = array[i];
+//     console.log('at index ${i}, the value is ${element}');
+//      //console.log("the index is " + i + " element of array" + arrA[i]);
+//     }
+// }
+// loopPrint(arrA);
+// loopPrint(fruits);
+//sum array and print the summ before function ennd
+const nums = [1,2,3,4,5,6,7,8,9];
+
+function sumList(array){
+    let sum = 0;
+    for (let i=0; i < array.length; i++){
+        const element = array[i];
+        sum = sum + element; 
+    }
+    console.log("sum of array elements: " + sum);
+}
+sumList(nums);
+sumList([1,2,3,4,5,6,7,8,9,10])
+
+//console.log(arrA);
+// console.log(arrA[0]);
+// console.log(arrA.length);
+
+
+
 
 //build array,start a inndex 1, add new item 7 ,try pushing some items to the array
 
@@ -32,5 +62,6 @@ console.log(arrA.length);
 // console.log(checkA);
 
 //const itemRemoved =arrA.pop90;\
-arrA.unshift(8);
-console.log(arrA);
+// arrA.unshift(8);
+// console.log(arrA);
+
--- class/compare.js
+++ class/compare.js
@@ -1,17 +1,46 @@
 //compare values
 //one qual sign :assignment operator
 
-//double equal sign: convert into same type (if diff) then compare
-console.log("7" == 7);//true
-console.log(7==7);//true
-console.log(8===7);//false
+// //double equal sign: convert into same type (if diff) then compare
+// console.log("7" == 7);//true
+// console.log(7==7);//true
+// console.log(8===7);//false
 
 
-//triple equal sign:compare type(if diff type, returns false) then comares
-console.log("7" === 7);//false
-console.log(7===7);//true
-console.log(8===7);//false
+// //triple equal sign:compare type(if diff type, returns false) then comares
+// console.log("7" === 7);//false
+// console.log(7===7);//true
+// console.log(8===7);//false
 
-//other primitives
-console.log(0 === false);
-console.log (0 == false);
+// //other primitives
+// console.log(0 === false);
+// console.log (0 == false);
+
+// //compare primitives
+// let a = 5;
+// let b = a; //5
+// let c = 5 ;
+
+// console.log("a=== c",a === c) ; //true
+// console.log("a === b",a === b ) ;//true
+// console.log("b === c",b === c) ;//true
+// a = 10;
+// console.log("a=== c",a === c) ;//10 === 5 false
+// console.log("a === b",a === b ) ; //10 === 5 false (b is copied -by-value ONCE)
+// console.log("b === c",b === c) ; // 5 === 5 true
+
+//comparing arrays (copy -by-value, but copying the reference memory address copy -by -reference value)
+ const arrA = [10]; // "adb457n"
+ const arrB = arrA; // "adb457n"
+ const arrC = [10]; // "efg156y"
+ const arrD = [10]; // "fdsfh17"
+
+ console.log(arrA === arrB);// memory address comparing "adb457n" === "adb457n" true
+ console.log(arrA === arrC); // "adb457n" === 
+ console.log(arrC === arrD);
+
+ arrA[0] = 100;
+ console.log(arrA); //[100]
+ console.log(arrB);//[100]
+ console.log(arrC);//[10]
+ console.log(arrA === arrB);// "adb457n" === "adb457n" true
