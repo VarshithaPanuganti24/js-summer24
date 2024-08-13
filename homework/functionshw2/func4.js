02eb0789481d5ef82275bac0029d1b12aa9807ac
Varshitha Panuganti <vpanuganti.1283@gmail.com>
2024-08-13 15:08:41 -0500

    updated

--- homework/functionshw2/func4.js
+++ homework/functionshw2/func4.js
@@ -1,15 +1,10 @@
 function countVowels(str) {
-
-    // Define a string containing all vowels
     const vowels = 'aAeEiIoOuU';
-
-    // Initialize a counter for vowels
     let vowelCount = 0;
 
-    // Iterate through the string and check if each character is a vowel
     for (let i = 0; i < str.length; i++) {
         if (vowels.includes(str[i])) {
-            vowelCount++;
+            vowelCount = vowelCount +1;
         }
     }
 
