a3358b85fee94201aca51ea1e8689b4ad979d930
Varshitha Panuganti <vpanuganti.1283@gmail.com>
2024-08-23 15:00:31 -0500

    1

--- homework/OOP3hw/oop4.js
+++ homework/OOP3hw/oop4.js
@@ -44,9 +44,9 @@ class CrewMember extends Employee {
     #manager;
   
     // Constructor 
-    constructor(name, id, email) {
+    constructor(name, id, email,manager) {
       super(name, id, email);
-      // this.#manager = manager;
+       this.#manager = manager;
     }
   
     setManager(manager) {
@@ -63,7 +63,7 @@ class Manager extends Employee {
     // Constructor 
     constructor(name, id, email) {
       super(name, id, email);
-      // this.#subordinates = subordinates;
+      //  this.#subordinates = subordinates;
     }
   
     addSubordinate(employee) {
@@ -79,31 +79,30 @@ class Manager extends Employee {
     }
 }
 const manager1 = new Manager("Mani", 1, "Mani@example.com");
-const manager2 = new Manager("Sunny", 2, "sunny@example.com");
-const manager3 = new Manager("Ann", 2, "Ann@example.com");
 
-const crewMember1 = new CrewMember("Akash", 2, "Aka@example.com");
-const crewMember2 = new CrewMember("lisa", 3, "lisa@example.com");
-const crewMember3 = new CrewMember("Davis", 4, "david@example.com");
+
+const crewMember1 = new CrewMember("Akash", 2, "Aka@example.com",manager1);
+const crewMember2 = new CrewMember("lisa", 3, "lisa@example.com",manager1);
+const crewMember3 = new CrewMember("Davis", 4, "david@example.com",manager1);
 
 //setting a manager to crew member
-crewMember1.setManager(manager1);
-crewMember2.setManager(manager2);
-crewMember3.setManager(manager1);
+// crewMember1.setManager(manager1);
+// crewMember2.setManager(manager2);
+// crewMember3.setManager(manager1);
 
 //console.log(manager1.getSubordinates());
 
 manager1.addSubordinate(crewMember1);
 manager1.addSubordinate(crewMember2);
-manager2.addSubordinate(crewMember3);
+manager1.addSubordinate(crewMember3);
 
 console.log(manager1.getSubordinates());
 console.log(manager1.getSubordinates().map(sub => sub.getName()));//getiing names of crew members
 
 console.log(manager1.getSubordinates().map(sub => sub.getId())); //get id of crew members
 
-console.log(crewMember1.getManager().getName());
-crewMember1.setManager(manager3);
+//console.log(crewMember1.getManager().getName());
+//crewMember1.setManager(manager3);
 console.log(crewMember1.getManager().getName());
 console.log(crewMember2.getManager().getName());
 
