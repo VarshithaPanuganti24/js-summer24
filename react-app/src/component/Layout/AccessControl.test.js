64204faa9e0d3c2493e398c120be6bac98be4ed2
Varshitha Panuganti <vpanuganti.1283@gmail.com>
2024-10-25 17:06:04 -0500

    memory

--- react-app/src/component/Layout/AccessControl.test.js
+++ react-app/src/component/Layout/AccessControl.test.js
@@ -1,45 +1,46 @@
-import { render ,screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import AccessControl, { checkAccess } from "./AccessControl";
- 
+import { expect, test } from "@jest/globals";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import Unauthorized from "../../pages/Unauthorised";
 
 describe("checkAccess function works properly", () => {
-	it("should allow access if requiredRoles is not provided", () => {
-		// arrange
-		const hasAccess = checkAccess("user");
-		// assert
-		expect(hasAccess).toBe(true);
-	});
+    it("should allow access if requiredRoles is not provided", () => {
+        // arrange
+        const hasAccess = checkAccess("user");
+        // assert
+        expect(hasAccess).toBe(true);
+    });
 
-	it("should allow access if role is in requiredRoles", () => {
-		const role = "admin";
-		const requiredRoles = ["admin", "super-admin"];
-		const hasAccess = checkAccess(role, requiredRoles);
-		expect(hasAccess).toBe(true);
-	});
-
-	it("should not allow access if role is not provided, but requiredRole is present", () => {
+    it("should allow access if role is in requiredRoles", () => {
         const role = "admin";
-		const requiredRoles = ["admin", "super-admin"];
-		const hasAccess = checkAccess(requiredRoles);
-		expect(!hasAccess).toBe(true);
+        const requiredRoles = ["admin", "super-admin"];
+        const hasAccess = checkAccess(role, requiredRoles);
+        expect(hasAccess).toBe(true);
     });
 
-	// 
-    it("role & requireRoles is given but role is not present in requireRoles", ()=>{
-
-        const role = "user";
-		const requiredRoles = ["admin", "super-admin"];
-		const hasAccess = checkAccess( role,requiredRoles);
-		expect(!hasAccess).toBe(true);
+    it("should not allow access if role is not provided, but requiredRole is present", () => {
+        const role = undefined;
+        const requiredRoles = ["admin", "super-admin"];
+        const hasAccess = checkAccess(role, requiredRoles);
+        expect(hasAccess).toBe(false);
     });
-    
 
-	// 
-    if("additional case of no role and no requireRoles",() => {
+    // 
+    it("role & requireRoles is given but role is not present in requireRoles", () => {
+
         const role = "user";
-		const requiredRoles = ["admin", "super-admin"];
-		const hasAccess = checkAccess();
-		expect(!hasAccess).toBe(true);
+        const requiredRoles = ["admin", "super-admin"];
+        const hasAccess = checkAccess(role, requiredRoles);
+        expect(hasAccess).toBe(false);
+    });
+
+
+    // additional case of no role and no requireRoles
+    it("should allow if no arguments are passed to checkAccess ", () => {
+
+        const hasAccess = checkAccess();
+        expect(hasAccess).toBe(true);
     });
 
 });
@@ -47,10 +48,10 @@ describe("checkAccess function works properly", () => {
 
 
 describe("AccessControl component", () => {
-    it("should render non-page children component with proper role", () => {
+    it("should render when user has acess and  children is not a page", () => {
         //arrange
         render(
-            <AccessControl role="user" requiredRole={["user","admin"]} isPage={false} >
+            <AccessControl role="user" requiredRoles={["user", "admin"]} isPage={false} >
                 <h1>Hello World</h1>
             </AccessControl>
         );
@@ -62,6 +63,60 @@ describe("AccessControl component", () => {
         expect(h1Element).toHaveTextContent("Hello World");
 
     });
+    //hw
+    it("should render when user has access and children is a page", () => {
+        render(
+            <AccessControl role="user" requiredRole={["user", "admin"]} isPage={true} >
+                <input placeholder="Hello" />
+            </AccessControl>
+        );
+        //act
 
-    
+        //assert
+        // const h1Element = screen.getByRole("heading");
+        // expect(h1Element).toBeInTheDocument();
+        // expect(h1Element).toHaveTextContent("Hello World");
+
+        const inputNode = screen.getByPlaceholderText("Hello");
+        expect(inputNode).toBeInTheDocument();
+    });
+
+    it("should navigate to unauthorized page when children is a page and user doent have access", () => {
+        const testPage = (
+            <AccessControl role="user" requiredRoles={["super-admin", "admin"]} isPage={true} >
+                <div>Mock page </div>
+            </AccessControl>
+        );
+
+        render(
+            <MemoryRouter initialEntries={["/test"]}>  
+                <Routes>
+                    <Route path="test" element={testPage} />
+                    <Route path="unauthorized" element={<Unauthorized />} />
+
+                </Routes>
+            </MemoryRouter>
+        );
+        //act
+
+        //assert
+        const mockPage = screen.queryByText("Mock Page");
+        expect(mockPage).not.toBeInTheDocument();
+
+        //assert
+        const unauthorized = screen.queryByTestId("unauthorized-page");
+        expect(unauthorized).toBeInTheDocument();
+    });
+
+    it("should render null if user doent have access and children is not a page", () => {
+        render(
+            <AccessControl role="guest" requiredRoles={["user", "admin"]} isPage={false} >
+                <button>Submit</button>
+            </AccessControl>
+        )
+        const buttonElement = screen.queryByText("button");
+        expect(buttonElement).not.toBeInTheDocument();
+    });
+
+
 });
--- react-app/src/component/Layout/AuthCannotAccess.jsx
+++ react-app/src/component/Layout/AuthCannotAccess.jsx
@@ -2,31 +2,31 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 
+const AuthCannotAccess = (props) => {
+  //const children = props.children;
+  //console.log(props);
+  const { children } = props;
+  const { user } = useAuth();
+  const isAuthenticated = user || user?.id > 0 || user?.accessToken;
 
-const AuthCannotAccess = (props ) => {
-    //const children = props.children;
-    //console.log(props);
-     const { children } = props;
-     const  { user } = useAuth();
-     const  isAuthenticated = user || user?.id > 0 || user?.accessToken;
+  //  const contextValue = useAuth();
+  //  const user = contextValue.user;
+  //  const {user}= contextValue;
 
-    //  const contextValue = useAuth();
-    //  const user = contextValue.user;
-    //  const {user}= contextValue;
+  if (isAuthenticated) return <Navigate to="/dashboard" replace />;
 
-     
-    if (isAuthenticated) return <Navigate to ="/dashboard" replace />;
-    
-    return children ;
- 
+  return children;
 };
 
 export default AuthCannotAccess;
-
-
 
 //when login we need to redirect link the /logic to AuthCannotAccess
 //where the Auth login checks if user is login in or not if not logged in login abd register
 //get the user object without destructuring
 // as system to control some access to components based on roles
 //take the role check if he has permission from other role to render the button for amatching role
+
+//authcannotaccess
+//navbar
+//privatebar
+//private routes
